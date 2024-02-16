7d3d3d100e6703262eeb45617762ed1314f494b7
Ye Thatoe Kyaw <thatoe@pm.me>
2024-02-16 11:47:51 +0800

    add home-manager

--- flake.nix
+++ flake.nix
@@ -1,13 +1,27 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
       alderaan = nixpkgs.lib.nixosSystem {
         modules = [
           ./hosts/alderaan
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      "thatoe@alderaan" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home/alderaan
         ];
       };
     };
