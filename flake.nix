b3863ed0344d413c2e765afadf3ba08d62a83921
Ye Thatoe Kyaw <thatoe@pm.me>
2024-12-04 23:11:53 +0800

    set up lsp and formatter and clean up

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-homebrew = {
       url = "github:zhaofengli-wip/nix-homebrew";
       inputs = {
@@ -26,84 +30,85 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle }:
-  let
-    configuration = { config, pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      homebrew-bundle,
+    }:
+    let
+      configuration =
+        { config, ... }:
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        {
+          nix.settings.experimental-features = "nix-command flakes";
 
-      nixpkgs.config.allowUnfree = true;
+          nixpkgs = {
+            hostPlatform = "aarch64-darwin";
+            config.allowUnfree = true;
+          };
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+          system = {
+            configurationRevision = self.rev or self.dirtyRev or null;
+            stateVersion = 5;
+          };
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      homebrew = {
-        enable = true;
-        casks = [
-          "android-studio"
-          "caffeine"
-          "docker"
-          "eloston-chromium"
-          "flutter"
-          "intune-company-portal"
-          "microsoft-auto-update"
-          "microsoft-teams"
-        ];
-        masApps = {
-          Xcode = 497799835;
+          homebrew = {
+            enable = true;
+            casks = [
+              "android-studio"
+              "caffeine"
+              "docker"
+              "eloston-chromium"
+              "flutter"
+              "intune-company-portal"
+              "microsoft-auto-update"
+              "microsoft-teams"
+            ];
+            masApps = {
+              Xcode = 497799835;
+            };
+            global.autoUpdate = false;
+            onActivation.cleanup = "zap";
+            taps = builtins.attrNames config.nix-homebrew.taps;
+          };
         };
-        global.autoUpdate = false;
-        onActivation.cleanup = "zap";
-        taps = builtins.attrNames config.nix-homebrew.taps;
+    in
+    {
+      darwinConfigurations."macalania" = nix-darwin.lib.darwinSystem {
+        modules = [
+          home-manager.darwinModules.home-manager
+          {
+            users.users.thatoe = {
+              name = "thatoe";
+              home = "/Users/thatoe";
+            };
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.thatoe = import ./home.nix;
+            };
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              user = "thatoe";
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+              };
+              mutableTaps = false;
+            };
+          }
+          configuration
+        ];
       };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#macalania
-    darwinConfigurations."macalania" = nix-darwin.lib.darwinSystem {
-      modules = [
-        home-manager.darwinModules.home-manager
-        {
-          users.users.thatoe = {
-            name = "thatoe";
-            home = "/Users/thatoe";
-          };
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.thatoe = import ./home.nix;
-        }
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            user = "thatoe";
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
-            };
-            mutableTaps = false;
-          };
-        }
-        configuration
-      ];
-    };
-  };
 }
