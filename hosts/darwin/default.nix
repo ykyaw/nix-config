c9d4068ffa39512566a782ccc63da889a492e11e
Ye Thatoe Kyaw <thatoe@pm.me>
2024-12-04 23:29:31 +0800

    configure dock

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,4 +1,9 @@
-{ config, self, ... }:
+{
+  config,
+  pkgs,
+  self,
+  ...
+}:
 
 {
   nix.settings.experimental-features = "nix-command flakes";
@@ -11,6 +16,24 @@
   system = {
     configurationRevision = self.rev or self.dirtyRev or null;
     stateVersion = 5;
+    defaults.dock = {
+      autohide = true;
+      mru-spaces = false;
+      persistent-apps = [
+        "${pkgs.brave}/Applications/Brave Browser.app"
+        "/Applications/Chromium.app"
+        "${pkgs.kitty}/Applications/kitty.app"
+        "${pkgs.vscode}/Applications/Visual Studio Code.app"
+        "${pkgs.gitkraken}/Applications/GitKraken.app"
+        "${pkgs.dbeaver-bin}/Applications/DBeaver.app"
+        "/Applications/Microsoft Teams.app"
+        "${pkgs.spotify}/Applications/Spotify.app"
+      ];
+      persistent-others = [
+        "/Users/thatoe/Downloads"
+      ];
+      show-recents = false;
+    };
   };
 
   users.users.thatoe = {
