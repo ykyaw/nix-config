9ab53086612186067d530924da80020c84e45747
Ye Thatoe Kyaw <thatoe@pm.me>
2024-12-14 22:02:04 +0800

    init nixos

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.zanarkand = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./hosts/nixos
+      ];
+    };
+  };
+}
