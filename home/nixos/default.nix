acde4bf65f3d821dc41a7e7dd1271434ad325046
Ye Thatoe Kyaw <thatoe@pm.me>
2024-12-15 10:45:39 +0800

    set up kitty

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -27,6 +27,16 @@
         };
     };
     fzf.enable = true;
+    kitty = {
+      enable = true;
+      font.name = "Comic Code Ligatures";
+      settings = {
+        window_margin_width = 5;
+        background_opacity = 0.9;
+        background_blur = 32;
+      };
+      themeFile = "tokyo_night_night";
+    };
     starship = {
       enable = true;
       settings.add_newline = false;
