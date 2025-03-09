c0b887567e51c67217f8c61eab65cbbd54d8aebb
Ye Thatoe Kyaw <thatoe@pm.me>
2025-03-09 21:01:24 +0800

    ✨ add git config

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -45,6 +45,20 @@
         theme = "tokyonight";
       };
     };
+    git = {
+      enable = true;
+      userEmail = "thatoe@pm.me";
+      userName = "Ye Thatoe Kyaw";
+      signing = {
+        signByDefault = true;
+        key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      };
+      extraConfig = {
+        gpg.format = "ssh";
+        push.autoSetupRemote = true;
+        tag.forceSignAnnotated = true;
+      };
+    };
     fish = {
       enable = true;
       interactiveShellInit = ''
