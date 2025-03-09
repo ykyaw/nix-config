3cea4db6e76661cca8ed748d3e96f04643b005de
Ye Thatoe Kyaw <thatoe@pm.me>
2025-03-09 20:35:35 +0800

    ✨ set fish as default interactive shell

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home = {
@@ -11,5 +11,32 @@
     ];
   };
 
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+    bash = {
+      enable = true;
+      initExtra = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      '';
+    };
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+        fish_vi_key_bindings
+      '';
+      shellAliases =
+        let
+          flake = "${config.home.homeDirectory}/development/nix-config";
+        in
+        {
+          ns = "sudo nixos-rebuild switch --flake ${flake}";
+          nu = "nix flake update --flake ${flake}";
+        };
+    };
+  };
 }
