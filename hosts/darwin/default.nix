5b9ce003709e75300d62a763d443ed0e58fd0965
Ye Thatoe Kyaw <thatoe@pm.me>
2025-03-10 16:50:39 +0800

    ✨ add darwin defaults and keyboard settings

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,4 +1,9 @@
-{ config, inputs, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 
 {
   imports = [
@@ -42,5 +47,51 @@
   system = {
     configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
     stateVersion = 6;
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        ApplePressAndHoldEnabled = false;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSWindowShouldDragOnGesture = true;
+      };
+      dock = {
+        autohide = true;
+        mru-spaces = false;
+        persistent-apps = [
+          { app = "${pkgs.brave}/Applications/Brave Browser.app"; }
+          { app = "/Applications/Microsoft Outlook.app"; }
+          { app = "/Applications/Ghostty.app"; }
+          { app = "${pkgs.vscode}/Applications/Visual Studio Code.app"; }
+          { app = "${pkgs.postman}/Applications/Postman.app"; }
+          { app = "${pkgs.dbeaver-bin}/Applications/dbeaver.app"; }
+          { app = "${pkgs.gitkraken}/Applications/GitKraken.app"; }
+          { app = "/Applications/Microsoft Teams.app"; }
+          { app = "${pkgs.spotify}/Applications/Spotify.app"; }
+        ];
+        persistent-others = [
+          "/Users/thatoe/Downloads"
+        ];
+        show-recents = false;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
   };
 }
