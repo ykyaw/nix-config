2d49b8d52dda71bbd4fb41f68522297a9792aedf
Ye Thatoe Kyaw <thatoe@pm.me>
2025-07-19 22:52:56 +0800

    add nixos-hardware module

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
     impermanence.url = "github:nix-community/impermanence";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,6 +14,7 @@
   outputs =
     {
       nixpkgs,
+      nixos-hardware,
       impermanence,
       home-manager,
       ...
@@ -20,18 +22,27 @@
 
     {
       nixosConfigurations.zanarkand = nixpkgs.lib.nixosSystem {
-        modules = [
-          impermanence.nixosModules.impermanence
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.thatoe = import ./home.nix;
-            };
-          }
-          ./configuration.nix
-        ];
+        modules =
+          with nixos-hardware.nixosModules;
+          [
+            common-pc
+            common-pc-ssd
+            common-cpu-amd
+            common-cpu-amd-pstate
+            common-gpu-amd
+          ]
+          ++ [
+            impermanence.nixosModules.impermanence
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.thatoe = import ./home.nix;
+              };
+            }
+            ./configuration.nix
+          ];
       };
     };
 }
