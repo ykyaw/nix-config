ae384f5130cd4980ef08d9cd00c9a0a5977bee12
Ye Thatoe Kyaw <thatoe@pm.me>
2025-07-19 23:05:21 +0800

    enable automatic store gc and optimisation

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,15 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
-  nix.settings.experimental-features = "nix-command flakes";
+  nix = {
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+    optimise.automatic = true;
+    settings.experimental-features = "nix-command flakes";
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   boot = {
