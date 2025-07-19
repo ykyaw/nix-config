64035e2f907aaafb113f9aedae74e97fcb88a877
Ye Thatoe Kyaw <thatoe@pm.me>
2025-07-19 23:08:23 +0800

    enable fish shell

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home = {
@@ -13,4 +13,22 @@
   };
 
   programs.home-manager.enable = true;
+
+  programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+        fish_vi_key_bindings
+      '';
+      shellAliases =
+        let
+          flake = "${config.home.homeDirectory}/development/nix-config";
+        in
+        {
+          ns = "sudo nixos-rebuild switch --flake ${flake}";
+          nu = "nix flake update --flake ${flake}";
+        };
+    };
+  };
 }
