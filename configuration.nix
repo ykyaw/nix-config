7e0f3744bc85d57e887142deeaf861c9514c7b35
Ye Thatoe Kyaw <thatoe@pm.me>
2025-07-19 23:07:38 +0800

    harden openssh

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,13 @@
   time.timeZone = "Asia/Singapore";
 
   services = {
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+      };
+    };
     pipewire = {
       enable = true;
       pulse.enable = true;
