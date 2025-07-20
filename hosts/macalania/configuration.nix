3151168498678743027706463d89da038e645989
Ye Thatoe Kyaw <thatoe@pm.me>
2025-07-20 16:37:31 +0800

    add darwin defaults settings

--- hosts/macalania/configuration.nix
+++ hosts/macalania/configuration.nix
@@ -1,4 +1,9 @@
-{ config, self, ... }:
+{
+  config,
+  pkgs,
+  self,
+  ...
+}:
 
 {
   imports = [
@@ -13,6 +18,49 @@
     configurationRevision = self.rev or self.dirtyRev or null;
     stateVersion = 6;
     primaryUser = "thatoe";
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        ApplePressAndHoldEnabled = false;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        InitialKeyRepeat = 25;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSWindowShouldDragOnGesture = true;
+      };
+      dock = {
+        autohide = true;
+        mru-spaces = false;
+        persistent-apps = [
+          "${pkgs.brave}/Applications/Brave Browser.app"
+          "${pkgs.ghostty-bin}/Applications/Ghostty.app"
+          "${pkgs.vscode}/Applications/Visual Studio Code.app"
+          "${pkgs.dbeaver-bin}/Applications/dbeaver.app"
+          "/Applications/Microsoft Teams.app"
+          "${pkgs.spotify}/Applications/Spotify.app"
+        ];
+        persistent-others = [
+          "${config.users.users.thatoe.home}/Downloads"
+        ];
+        show-recents = false;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
   };
 
   users.users.thatoe.home = "/Users/thatoe";
