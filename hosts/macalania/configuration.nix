895c6d42d5f381fe958bfe4ec7e5aff48a992570
Ye Thatoe Kyaw <thatoe@pm.me>
2025-07-20 17:44:31 +0800

    add aerospace config

--- hosts/macalania/configuration.nix
+++ hosts/macalania/configuration.nix
@@ -65,6 +65,127 @@
 
   users.users.thatoe.home = "/Users/thatoe";
 
+  services.aerospace = {
+    enable = true;
+    settings = {
+      enable-normalization-flatten-containers = false;
+      enable-normalization-opposite-orientation-for-nested-containers = false;
+      accordion-padding = 0;
+      on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+      mode.main.binding = {
+        alt-enter = "exec-and-forget open -a Ghostty";
+        alt-b = "exec-and-forget open -a 'Brave Browser'";
+        alt-c = "exec-and-forget open -a 'Visual Studio Code'";
+
+        alt-shift-q = "close";
+
+        alt-h = "focus left";
+        alt-j = "focus down";
+        alt-k = "focus up";
+        alt-l = "focus right";
+
+        alt-left = "focus left";
+        alt-down = "focus down";
+        alt-up = "focus up";
+        alt-right = "focus right";
+
+        alt-shift-j = "move left";
+        alt-shift-k = "move down";
+        alt-shift-l = "move right";
+        alt-shift-h = "move left";
+
+        alt-shift-left = "move left";
+        alt-shift-down = "move down";
+        alt-shift-up = "move up";
+        alt-shift-right = "move right";
+
+        alt-s = "layout v_accordion";
+        alt-w = "layout h_accordion";
+        alt-e = "layout tiles horizontal vertical";
+
+        alt-f = "fullscreen";
+        alt-shift-space = "layout floating tiling";
+
+        alt-tab = "workspace-back-and-forth";
+
+        alt-1 = "workspace 1";
+        alt-2 = "workspace 2";
+        alt-3 = "workspace 3";
+        alt-4 = "workspace 4";
+        alt-5 = "workspace 5";
+        alt-6 = "workspace 6";
+        alt-7 = "workspace 7";
+        alt-8 = "workspace 8";
+        alt-9 = "workspace 9";
+        alt-0 = "workspace 10";
+
+        alt-shift-1 = "move-node-to-workspace 1";
+        alt-shift-2 = "move-node-to-workspace 2";
+        alt-shift-3 = "move-node-to-workspace 3";
+        alt-shift-4 = "move-node-to-workspace 4";
+        alt-shift-5 = "move-node-to-workspace 5";
+        alt-shift-6 = "move-node-to-workspace 6";
+        alt-shift-7 = "move-node-to-workspace 7";
+        alt-shift-8 = "move-node-to-workspace 8";
+        alt-shift-9 = "move-node-to-workspace 9";
+        alt-shift-0 = "move-node-to-workspace 10";
+
+        alt-shift-c = "reload-config";
+
+        alt-shift-s = "exec-and-forget screencapture -i -c";
+
+        alt-r = "mode resize";
+      };
+      mode.resize.binding = {
+        h = "resize width -50";
+        j = "resize height +50";
+        k = "resize height -50";
+        l = "resize width +50";
+        enter = "mode main";
+        esc = "mode main";
+      };
+      on-window-detected = [
+        {
+          "if".app-id = "com.brave.Browser";
+          run = [ "move-node-to-workspace 1" ];
+        }
+        {
+          "if".app-id = "com.microsoft.VSCode";
+          run = [ "move-node-to-workspace 2" ];
+        }
+        {
+          "if".app-id = "org.jkiss.dbeaver.core.product";
+          run = [ "move-node-to-workspace 3" ];
+        }
+        {
+          "if".app-id = "com.usebruno.app";
+          run = [ "move-node-to-workspace 3" ];
+        }
+        {
+          "if".app-id = "net.novade.novadeLite";
+          run = [ "move-node-to-workspace 3" ];
+        }
+        {
+          "if".app-id = "com.microsoft.teams2";
+          run = [ "move-node-to-workspace 4" ];
+        }
+        {
+          "if".app-id = "com.spotify.client";
+          run = [ "move-node-to-workspace 5" ];
+        }
+        {
+          "if".app-id = "com.mitchellh.ghostty";
+          run = [ "layout floating" ];
+        }
+      ];
+      workspace-to-monitor-force-assignment = {
+        "1" = "secondary";
+        "3" = "secondary";
+        "4" = "secondary";
+      };
+    };
+  };
+
   homebrew = {
     enable = true;
     casks = [
