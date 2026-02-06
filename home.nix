37fbec2555b1348e79ee52df44302336711abcbd
Ye Thatoe Kyaw <thatoe@pm.me>
2026-02-06 22:09:33 +0800

    feat(git): configure git

--- home.nix
+++ home.nix
@@ -38,6 +38,21 @@
         theme = "Catppuccin Mocha";
       };
     };
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          email = "thatoe@pm.me";
+          name = "Ye Thatoe Kyaw";
+        };
+        push.autoSetupRemote = true;
+      };
+      signing = {
+        format = "ssh";
+        key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        signByDefault = true;
+      };
+    };
     home-manager.enable = true;
     starship = {
       enable = true;
