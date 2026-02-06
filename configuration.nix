31cdd1472ea5ab21190c47d32b9acafaf0b61d1d
Ye Thatoe Kyaw <thatoe@pm.me>
2026-02-07 06:46:46 +0800

    gnome: add config

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,26 @@
     };
   };
 
+  environment = {
+    gnome.excludePackages = with pkgs; [
+      decibels
+      epiphany
+      gnome-console
+      gnome-contacts
+      gnome-maps
+      gnome-music
+      gnome-tour
+      showtime
+      snapshot
+    ];
+    systemPackages = with pkgs.gnomeExtensions; [
+      appindicator
+      blur-my-shell
+      caffeine
+      hot-edge
+    ];
+  };
+
   programs = {
     dconf.enable = true;
     steam = {
