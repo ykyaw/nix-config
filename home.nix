622a510cf8ceb02396ee22a12f6506ff2ce6b0c4
Ye Thatoe Kyaw <thatoe@pm.me>
2026-02-06 22:01:41 +0800

    fish: init

--- home.nix
+++ home.nix
@@ -1,3 +1,5 @@
+{ config, ... }:
+
 {
   home = {
     username = "thatoe";
@@ -5,5 +7,22 @@
     stateVersion = "25.11";
   };
 
-  programs.home-manager.enable = true;
+  programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+        fish_vi_key_bindings
+      '';
+      shellAliases =
+        let
+          flake = "${config.home.homeDirectory}/development/nix-config";
+        in
+        {
+          ns = "sudo nixos-rebuild switch --flake ${flake}";
+          nu = "nix flake update --flake ${flake}";
+        };
+    };
+    home-manager.enable = true;
+  };
 }
