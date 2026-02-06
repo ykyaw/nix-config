f5c05379e8d76872bdc91907acf93d6fbfc30b25
Ye Thatoe Kyaw <thatoe@pm.me>
2026-02-06 22:05:54 +0800

    🍱 add fonts and default fonts

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,25 @@
     };
   };
 
+  fonts = {
+    packages = with pkgs; [
+      dejavu_fonts
+      fira-code
+      inter
+      liberation_ttf
+      noto-fonts
+      noto-fonts-color-emoji
+    ];
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" ];
+      sansSerif = [ "Noto Sans" ];
+      monospace = [ "Berkeley Mono" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
+  environment.sessionVariables.FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+
   users = {
     mutableUsers = false;
     users.thatoe = {
