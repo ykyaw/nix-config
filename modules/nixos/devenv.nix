8cd2a94c603a4c959c261e553a9318fc47969558
Ye Thatoe Kyaw <thatoe@pm.me>
2026-02-07 11:43:30 +0800

    🔧 enable devenv

---
+++ modules/nixos/devenv.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = [ pkgs.devenv ];
+
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
