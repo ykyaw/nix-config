8ecc68e3144a382dfd9d19ed6f5b2a64c1046497
Ye Thatoe Kyaw <thatoe@pm.me>
2026-02-07 13:04:08 +0800

    gnome: add more keybindings

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -39,13 +39,53 @@
       screen-blank = "always";
       show-notifications = false;
     };
+    "org/gnome/system/location".enabled = true;
+    "org/gtk/settings/file-chooser".clock-format = "12h";
+
+    ## Keybindings
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>7" ];
+      switch-to-workspace-8 = [ "<Super>8" ];
+      switch-to-workspace-9 = [ "<Super>9" ];
+      move-to-workspace-1 = [ "<Super><Shift>1" ];
+      move-to-workspace-2 = [ "<Super><Shift>2" ];
+      move-to-workspace-3 = [ "<Super><Shift>3" ];
+      move-to-workspace-4 = [ "<Super><Shift>4" ];
+      move-to-workspace-5 = [ "<Super><Shift>5" ];
+      move-to-workspace-6 = [ "<Super><Shift>6" ];
+      move-to-workspace-7 = [ "<Super><Shift>7" ];
+      move-to-workspace-8 = [ "<Super><Shift>8" ];
+      move-to-workspace-9 = [ "<Super><Shift>9" ];
+      close = [
+        "<Super>Q"
+        "<Alt>F4"
+      ];
+      maximize = [
+        "<Super>F"
+        "<Super>Up"
+      ];
+    };
     "org/gnome/shell/keybindings" = {
       switch-to-application-1 = [ "<Super>E" ];
       switch-to-application-2 = [ "<Super>B" ];
       switch-to-application-3 = [ "<Super>Return" ];
       switch-to-application-4 = [ "<Super>C" ];
+      switch-to-application-5 = [ ];
+      switch-to-application-6 = [ ];
+      switch-to-application-7 = [ ];
+      switch-to-application-8 = [ ];
+      switch-to-application-9 = [ ];
     };
-    "org/gnome/system/location".enabled = true;
-    "org/gtk/settings/file-chooser".clock-format = "12h";
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "yazi";
+      command = "ghostty -e yazi";
+      binding = "<Super>Y";
+    };
   };
 }
