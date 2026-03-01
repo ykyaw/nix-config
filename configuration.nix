29222afe402d633c8800a144ba3adb7ede239565
Ye Thatoe Kyaw <thatoe@pm.me>
2026-03-01 15:29:09 +0800

    feat(brave): add brave config with extensions

--- configuration.nix
+++ configuration.nix
@@ -100,9 +100,6 @@
       isNormalUser = true;
       initialHashedPassword = "$y$j9T$lDwpaQZ/o0gUWs7it7bAM.$BxG4FDm/4aGnHYldH.M.0WopE4yaVDvi3W.wE2uVDQ7";
       extraGroups = [ "wheel" ];
-      packages = with pkgs; [
-        brave
-      ];
     };
   };
 
--- home.nix
+++ home.nix
@@ -14,6 +14,16 @@
   };
 
   programs = {
+    brave = {
+      enable = true;
+      extensions = [
+        { id = "dnhpnfgdlenaccegplpojghhmaamnnfp"; } # Augmented Steam
+        { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # Proton Pass: Free Password Manager
+        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock for YouTube - Skip Sponsorships
+        { id = "kdbmhfkmnlmbkgbabkdealhhbfhlmmon"; } # SteamDB
+        { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # Vimium
+      ];
+    };
     fish = {
       enable = true;
       interactiveShellInit = ''
