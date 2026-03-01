0fc53b1186b855b21194fb67d0a840094c33f001
Ye Thatoe Kyaw <thatoe@pm.me>
2026-03-01 14:46:26 +0800

    build: add home-manager module

--- flake.nix
+++ flake.nix
@@ -5,14 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     impermanence = {
       url = "github:nix-community/impermanence";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, impermanence }: {
+  outputs = { self, nixpkgs, impermanence, home-manager }: {
     nixosConfigurations.zanarkand = nixpkgs.lib.nixosSystem {
       modules = [
         impermanence.nixosModules.impermanence
+        home-manager.nixosModules.home-manager
         ./configuration.nix
       ];
     };
