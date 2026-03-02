6ba5403cd7aa375fc9ad537355470b34ee48b8f5
Ye Thatoe Kyaw <thatoe@pm.me>
2026-03-02 17:39:10 +0800

    feat(devenv): enable devenv and direnv

---
+++ modules/features/development/devenv.nix
@@ -0,0 +1,21 @@
+{
+  flake.modules = {
+    nixos.development = {
+      nix.extraOptions = ''
+        extra-substituters = https://devenv.cachix.org
+        extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+      '';
+    };
+
+    homeManager.development =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.devenv ];
+
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+      };
+  };
+}
