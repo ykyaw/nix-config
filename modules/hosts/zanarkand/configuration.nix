362dcb6883fbe84c49a236d98aa9ec3f389ccd8c
Ye Thatoe Kyaw <thatoe@pm.me>
2026-03-05 21:28:07 +0800

    fix: add temporary workaround for system freezes

--- modules/hosts/zanarkand/configuration.nix
+++ modules/hosts/zanarkand/configuration.nix
@@ -10,5 +10,11 @@
     ];
 
     networking.hostName = "zanarkand";
+
+    # Temporary workaround for system freezes.
+    # https://bugzilla.kernel.org/show_bug.cgi?id=221103
+    services.udev.extraRules = ''
+      ACTION=="add|change", SUBSYSTEM=="pci", KERNEL=="0000:6c:00.4", ATTR{power/control}="on"
+    '';
   };
 }
