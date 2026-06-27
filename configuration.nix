63b68e1c7fdd151c7a200a803939cf47550d9b99
Ye Thatoe Kyaw <thatoe@pm.me>
2026-06-27 10:40:04 +0200

    configure default fonts

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,20 @@
     };
   };
 
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-color-emoji
+    ];
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" ];
+      sansSerif = [ "Noto Sans" ];
+      monospace = [ "Berkeley Mono" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
   users = {
     mutableUsers = false;
     users.thatoe = {
