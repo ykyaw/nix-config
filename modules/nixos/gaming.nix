918b04028d4590c753f5574c281e8e18082b7dd7
Ye Thatoe Kyaw <thatoe@pm.me>
2026-06-28 12:26:12 +0200

    enable xone

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -3,17 +3,18 @@
     allowedUnfreePackages = [
       "steam"
       "steam-unwrapped"
+      "xone-dongle-firmware"
     ];
   };
 
-  flake.modules.nixos.gaming =
-    { pkgs, ... }:
-    {
-      programs.steam = {
-        enable = true;
-        extraCompatPackages = [ pkgs.proton-ge-bin ];
-      };
-
-      environment.systemPackages = [ pkgs.xclicker ];
+  flake.modules.nixos.gaming = { pkgs, ... }: {
+    programs.steam = {
+      enable = true;
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
+
+    hardware.xone.enable = true;
+
+    environment.systemPackages = [ pkgs.xclicker ];
+  };
 }
