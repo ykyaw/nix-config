5f8e9d0c711ece6db578a67d34b8d4cb0a1cbc0a
Ye Thatoe Kyaw <thatoe@pm.me>
2026-07-06 12:56:47 +0200

    add home-manager module

--- configuration.nix
+++ configuration.nix
@@ -138,5 +138,10 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.05"; # Did you read the comment?
 
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.thatoe = ./home.nix;
+  };
 }
 
