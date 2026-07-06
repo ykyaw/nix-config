a4f687770a23496a3193b7a2ffac89cd615c7304
Ye Thatoe Kyaw <thatoe@pm.me>
2026-07-06 11:07:00 +0200

    nix flake init

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.zanarkand = nixpkgs.lib.nixosSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
