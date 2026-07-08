c3821dae3adfbcba514dba368abab256fe615c20
Ye Thatoe Kyaw <thatoe@pm.me>
2026-07-08 11:08:08 +0200

    add android-studio

--- modules/home/apps.nix
+++ modules/home/apps.nix
@@ -1,11 +1,13 @@
 {
   allowedUnfreePackages = [
+    "android-studio"
     "discord"
     "spotify"
   ];
 
   flake.modules.homeManager.apps = { pkgs, ... }: {
     home.packages = with pkgs; [
+      android-studio
       bruno
       dbeaver-bin
       discord
