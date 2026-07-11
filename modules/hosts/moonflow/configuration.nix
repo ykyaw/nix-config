e74c13ca50ac2af6703ada80d1c3f68ed2a14de3
Ye Thatoe Kyaw <thatoe@pm.me>
2026-07-11 17:13:39 +0200

    add new host for laptop

---
+++ modules/hosts/moonflow/configuration.nix
@@ -0,0 +1,53 @@
+{ config, inputs, ... }:
+let
+  hostName = "moonflow";
+in
+{
+  flake.nixosConfigurations = inputs.self.lib.mkNixos hostName;
+
+  flake.modules.nixos.${hostName} = {
+    imports = with inputs.self.modules.nixos; [
+      boot
+      plymouth
+      nix
+      unfree
+      impermanence
+      btrbk
+      users
+      home-manager
+      networking
+      ssh
+      sops
+      locale
+      audio
+      fonts
+      gnome
+      firefox
+      docker
+      gaming
+    ];
+
+    networking = { inherit hostName; };
+
+    home-manager.users.${config.username} = {
+      imports = with inputs.self.modules.homeManager; [
+        home-manager
+        sops
+        nix-index
+        themes
+        shell
+        direnv
+        git
+        neovim
+        ghostty
+        vscodium
+        ai
+        apps
+      ];
+
+      home.stateVersion = "26.05";
+    };
+
+    system.stateVersion = "26.05";
+  };
+}
